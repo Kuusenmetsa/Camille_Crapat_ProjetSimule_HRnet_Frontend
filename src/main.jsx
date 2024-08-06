b2d7be9ff51ace92688c2e48fd10071631c21157
Camille CRAPAT <camille.capt@gmail.com>
2024-08-06 08:54:00 +0200

    Initialisation des routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import './global.css';
+
+import Home from './Home';
+import Employees from './Employees';
+import Error from './Error';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<Router>
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/employees' element={<Employees />} />
+				<Route path='*' element={<Error />} />
+			</Routes>
+		</Router>
+	</React.StrictMode>
+);
