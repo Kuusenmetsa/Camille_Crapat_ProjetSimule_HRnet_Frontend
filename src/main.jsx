f311f0554bd27e0d983fb5879e7274685e5361e4
Camille CRAPAT <camille.capt@gmail.com>
2024-08-06 16:44:21 +0200

    Création du header

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 
 import './global.css';
 
+import Header from './components/Header';
 import Home from './pages/Home';
 import Employees from './pages/Employees';
 import Error from './pages/Error';
@@ -15,6 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<Provider store={store}>
 			<Router>
+				<Header />
 				<Routes>
 					<Route path='/' element={<Home />} />
 					<Route path='/employees' element={<Employees />} />
