232b9f24f0c34022574ee7e7197f2ca5755e102f
Camille CRAPAT <camille.capt@gmail.com>
2024-09-26 17:10:04 +0200

    feat (header): add header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,31 @@
+import { Link, useLocation } from 'react-router-dom';
+
 import './index.css';
 
+import Logo from '../../assets/img/logo.png';
+
 export default function Header() {
-	return <header></header>;
+	const location = useLocation();
+	return (
+		<header>
+			<div className='logo'>
+				<img src={Logo} alt='logo de WealthHealth' className='logo__image' />
+				<h1 className='logo__title'>HRnet</h1>
+			</div>
+			<nav>
+				<ul>
+					<li>
+						<Link to='/' className={location.pathname === '/' ? `link--selected` : ``}>
+							Create employee
+						</Link>
+					</li>
+					<li>
+						<Link to='/employees' className={location.pathname === '/employees' ? `link--selected` : ``}>
+							Current Employees
+						</Link>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
 }
