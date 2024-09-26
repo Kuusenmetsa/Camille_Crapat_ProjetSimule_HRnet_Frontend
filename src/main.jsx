dad5a6ca7c8c9f90b82e4becbbf6f56ef6c25c72
Camille CRAPAT <camille.capt@gmail.com>
2024-09-26 17:05:15 +0200

    feat (style): add global css

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import './global.css';
+
 import Header from './components/Header';
 import Home from './pages/Home';
 import Employees from './pages/Employees';
