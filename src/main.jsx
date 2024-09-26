b09f319d1b09e07788d8b68633553649554948fd
Camille CRAPAT <camille.capt@gmail.com>
2024-09-26 17:14:43 +0200

    feat (store): add store

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import './global.css';
 
@@ -9,15 +10,19 @@ import Home from './pages/Home';
 import Employees from './pages/Employees';
 import Error from './pages/Error';
 
+import { store } from './utils/Store';
+
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
-		<Router>
-			<Header />
-			<Routes>
-				<Route path='/' element={<Home />} />
-				<Route path='/employees' element={<Employees />} />
-				<Route path='*' element={<Error />} />
-			</Routes>
-		</Router>
+		<Provider store={store}>
+			<Router>
+				<Header />
+				<Routes>
+					<Route path='/' element={<Home />} />
+					<Route path='/employees' element={<Employees />} />
+					<Route path='*' element={<Error />} />
+				</Routes>
+			</Router>
+		</Provider>
 	</StrictMode>
 );
