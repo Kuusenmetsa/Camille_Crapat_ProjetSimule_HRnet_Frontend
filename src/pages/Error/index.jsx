dab377cc748ab3715899931c94a49a87b52c682f
Camille CRAPAT <camille.capt@gmail.com>
2024-09-26 17:33:45 +0200

    feat (emrror): add error page

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,5 +1,17 @@
+import { useEffect } from 'react';
+
 import './index.css';
 
 export default function Error() {
-	return <div></div>;
+	useEffect(() => {
+		document.title = 'HRnet | Error';
+	}, []);
+
+	return (
+		<div className='error'>
+			<p className='error__text'>Oups</p>
+			<p className='error__title'>404</p>
+			<p className='error__text'>La page que vous demandez est introuvable...</p>
+		</div>
+	);
 }
